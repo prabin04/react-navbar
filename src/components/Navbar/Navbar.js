283b67b6ca1e600c82913638895943a8ccebebc9
Prabin Parajuli <prabin.b9@gmail.com>
2020-10-13 11:51:10 +0200

    udpate

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
 
 class Navbar extends Component {
     render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React</h1>
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
                 <div className="menu-icon">
                     
                 </div>
                 <ul>
-                    <li><a href=""></a></li>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}><a className="{item.cName}" href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         )
     }
 }
+
+export default Navbar
